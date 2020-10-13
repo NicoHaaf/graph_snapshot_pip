2ae4bdb1074ea7964b7d09ba0a0bc4dca61f2a83
jakobus2205 <61044581+jakobus2205@users.noreply.github.com>
2020-10-13 08:36:22 +0200

    final version for release of 0.0.1

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="graph-snapshot-NicoHaaf", # Replace with your own username
+    name="graph-snapshot",
     version="0.0.1",
     author="Josua Kugler, Nico Haaf",
     author_email="josua.kugler@stud.uni-heidelberg.de, nico.haaf@stud.uni-heidelberg.de",
     description="A small package for creating snapshots of graphs with networkx",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/NicoHaaf/graph_snapshot_pip",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
